97ad391de0c458757351908f595f903aabb10809
VengefulNemesis <bakerjr78@gmail.com>
2023-07-22 18:34:15 -0400

    Stronger password generator

---
+++ Password Generator.py
@@ -0,0 +1,19 @@
+import random
+
+print ('Welcome To Your Password Generator')
+
+chars = 'abcdefghijklmonopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()'
+
+number = input ('Amount of passwords to generate: ')
+number = int(number)
+
+length = input('Input your password length: ')
+length = int(length)
+
+print('\nhere are you passwords:')
+
+for password in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+        print(passwords)
